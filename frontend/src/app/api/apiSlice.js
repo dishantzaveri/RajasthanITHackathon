c75e26e04afb82eef908c5884974dfef0bfb35be
dishantzaveri <zaveridishant@gmail.com>
2023-03-14 19:24:26 +0530

    setup

---
+++ frontend/src/app/api/apiSlice.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://127.0.0.1:8000',
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token
+        if (token) {
+            headers.set("Authorization", `Token ${token}`)
+        }
+        return headers
+    }
+})
+
+export const apiSlice = createApi({
+    baseQuery,
+    endpoints: builder => ({})
+})
