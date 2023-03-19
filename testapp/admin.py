bf6acb7dc8e5b768b581aaf7aa4fa66a5f852088
talkshrey <talkshrey@gmail.com>
2023-03-19 15:50:52 +0530

    landing page created

--- testapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Category)
-admin.site.register(Post_Like)
-admin.site.register(Session)
-admin.site.register(Attendee)
