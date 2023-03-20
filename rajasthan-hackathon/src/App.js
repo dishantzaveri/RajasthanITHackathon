6d97fb20cf972d8b265688450b004086017dc1d2
talkshrey <talkshrey@gmail.com>
2023-03-20 21:55:40 +0530

    cibil score

--- rajasthan-hackathon/src/App.js
+++ rajasthan-hackathon/src/App.js
@@ -6,9 +6,10 @@ import Landing from "./Pages/Landing";
 import Insurance from "./Pages/Insurance";
 import BusinessLoans from "./Pages/BusinessLoan";
 import Loans from "./Pages/Loans";
+import Cibil from "./Pages/Cibil";
 
 function App() {
-  return ( 
+  return (
     <div className="App">
       <Router>
         <Routes>
@@ -18,10 +19,11 @@ function App() {
           <Route path="/insurance" element={<Insurance />} />
           <Route path="/loans" element={<Loans />} />
           <Route path="/Loan" element={<BusinessLoans />} />
+          <Route path="/credit" element={<Cibil />} />
         </Routes>
       </Router>
     </div>
   );
 }
 
-export default App;    
+export default App;
