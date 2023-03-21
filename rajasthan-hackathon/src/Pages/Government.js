86b804e0a9a20ccac906471f1fac3152789c0e3c
lokita282 <lokitavarma282@gmail.com>
2023-03-21 05:34:19 +0530

    government functionality completed

--- rajasthan-hackathon/src/Pages/Government.js
+++ rajasthan-hackathon/src/Pages/Government.js
@@ -1,213 +1,222 @@
-import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
-import Divider from '@mui/material/Divider'
-import Drawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemText from '@mui/material/ListItemText'
-import MenuIcon from '@mui/icons-material/Menu'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 import Button from '@mui/material/Button'
-import logo from '../Images/logosb.png'
-import { Link } from 'react-router-dom'
-import Fade from '@mui/material/Fade'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import GovNavbar from '../Components/GovtNavbar'
 
-const drawerWidth = 240
-const navItems = ['Loan', 'Statistics']
 
-function DrawerAppBar(props) {
-  const { window } = props
-  const [mobileOpen, setMobileOpen] = React.useState(false)
 
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const [anchorEl1, setAnchorEl1] = React.useState(null)
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}))
 
-  const open = Boolean(anchorEl)
-  const open1 = Boolean(anchorEl1)
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}))
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState)
+function createData(name, loan, status) {
+  return { name, loan, status }
+}
+
+const rows = [
+  createData('Lokita Varma', 'Mudra'),
+  createData('Priyambi Hiran', 'CGTMSE'),
+  createData('Sarthak Bhan', 'CGTMSE'),
+  createData('Shrey Parekh', 'Mudra'),
+  createData('Priyambi Hiran', 'CGTMSE'),
+  createData('Sarthak Bhan', 'CGTMSE'),
+  createData('Shrey Parekh', 'Mudra'),
+  createData('Priyambi Hiran', 'CGTMSE'),
+  createData('Sarthak Bhan', 'CGTMSE'),
+]
+
+const rows2 = [
+  createData('Priyambi Hiran', 'CGTMSE'),
+  createData('Sarthak Bhan', 'CGTMSE'),
+  createData('Shrey Parekh', 'Mudra'),
+  createData('Priyambi Hiran', 'CGTMSE'),
+  createData('Sarthak Bhan', 'CGTMSE'),
+  createData('Shrey Parekh', 'Mudra'),
+  createData('Priyambi Hiran', 'CGTMSE'),
+  createData('Sarthak Bhan', 'CGTMSE'),
+]
+
+
+const Government = () => {
+  const [checked , setChecked ] = React.useState(false)
+  const handleClick = () => {
+    setChecked(true)
   }
-
-  const handleClick1 = (event) => {
-    setAnchorEl1(event.currentTarget)
-  }
-
-  const handleClose1 = () => {
-    setAnchorEl1(null)
-  }
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={'Loan'} sx={{ fontWeight: '10px' }} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  )
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined
-
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav" sx={{ bgcolor: '#D8E8EB', color: '#2F3A56' }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          <div
-            style={{
-              flexGrow: 1,
-              display: { xs: 'none', sm: 'block' },
-              textAlign: 'left',
-              padding: '10px',
-            }}
-          >
-            <Link to="http://localhost:3000/">
-              <img src={logo} alt="" />
-            </Link>
-          </div>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <Link to="http://localhost:3000/loans">
-              <Button
-                key={'Loan'}
-                sx={{ color: '#2F3A56', float: 'right !important' }}
-              >
-                <b>{'Loan'}</b>
-              </Button>
-            </Link>
-          </Box>
-          <Link to="http://localhost:3000/insurance">
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-              <Button
-                key={'Insurance'}
-                sx={{ color: '#2F3A56', float: 'right !important' }}
-              >
-                <b>{'Insurance'}</b>
-              </Button>
-            </Box>
-          </Link>
-          <Link to="http://localhost:3000/credit">
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-              <Button
-                key={'Credit Report'}
-                sx={{ color: '#2F3A56', float: 'right !important' }}
-              >
-                <b>{'Credit Report'}</b>
-              </Button>
-            </Box>
-          </Link>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <Button
-              onClick={handleClick1}
-              key={'Activities'}
-              sx={{ color: '#2F3A56', float: 'right !important' }}
-            >
-              <b>{'Activities'}</b>
-            </Button>
-          </Box>
-          <Menu
-            id="fade-menu"
-            fullwidth
-            MenuListProps={{
-              'aria-labelledby': 'fade-button',
-            }}
-            anchorEl={anchorEl1}
-            open={open1}
-            onClose={handleClose1}
-            TransitionComponent={Fade}
-            sx={{ margin: '10px', padding: '10px' }}
-          >
-            <Link
-              style={{ textDecoration: 'none', color: 'black' }}
-              to="http://127.0.0.1:3001/events"
-              target="_blank"
-            >
-              <MenuItem
-                sx={{
-                  margin: '10px',
-                  padding: '10px',
-                  width: '140px',
-                }}
-              >
-                Events
-              </MenuItem>
-            </Link>
-            <Link
-              style={{ textDecoration: 'none', color: 'black' }}
-              to="http://127.0.0.1:3001/campaigns"
-              target="_blank"
-            >
-              <MenuItem
-                sx={{
-                  margin: '10px',
-                  padding: '10px',
-                  width: '140px',
-                }}
-              >
-                Campaigns
-              </MenuItem>
-            </Link>
-            {/* <Link to="/marketplace"> */}
-            <MenuItem sx={{ margin: '10px', padding: '10px', width: '140px' }}>
-              Marketplace
-            </MenuItem>
-            {/* </Link> */}
-          </Menu>
-
-          {/* </div> */}
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-      </Box>
-    </Box>
+    <>
+      <GovNavbar />
+      <div
+        style={{
+          textAlign: 'left',
+          marginInline: '15%',
+          marginTop: '20px',
+          marginBottom: '20px',
+        }}
+      >
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell
+                  align="center"
+                  style={{
+                    color: 'white',
+                    backgroundColor: '#E24748',
+                    width: '450px',
+                  }}
+                >
+                  Name
+                </StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  style={{
+                    color: 'white',
+                    backgroundColor: '#E24748',
+                    width: '450px',
+                  }}
+                >
+                  Loan
+                </StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  style={{
+                    color: 'white',
+                    backgroundColor: '#E24748',
+                    width: '450px',
+                  }}
+                >
+                  Status
+                </StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {checked ? (
+                <>
+                  {rows2.map((row) => (
+                    <StyledTableRow key={row.name}>
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        align="center"
+                      >
+                        <a
+                          href="https://www.mudra.org.in/"
+                          style={{ textDecoration: 'none', color: '#2F3A56' }}
+                        >
+                          {row.name}
+                        </a>
+                      </StyledTableCell>
+                      <StyledTableCell align="center" sx={{ color: '#2F3A56' }}>
+                        {row.loan}
+                      </StyledTableCell>
+                      <StyledTableCell align="center" sx={{ color: '#2F3A56' }}>
+                        <Button
+                          variant="text"
+                          color="primary"
+                          sx={{
+                            backgroundColor: '#3A7E55',
+                            color: '#FFFFFF',
+                            margin: 1,
+                          }}
+                          onClick={handleClick}
+                        >
+                          Approve
+                        </Button>
+                        <Button
+                          variant="text"
+                          color="primary"
+                          sx={{
+                            backgroundColor: '#E0292A',
+                            color: '#FFFFFF',
+                            margin: 1,
+                          }}
+                        >
+                          Reject
+                        </Button>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </>
+              ) : (
+                <>
+                  {rows.map((row) => (
+                    <StyledTableRow key={row.name}>
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        align="center"
+                      >
+                        <a
+                          href="https://www.mudra.org.in/"
+                          style={{ textDecoration: 'none', color: '#2F3A56' }}
+                        >
+                          {row.name}
+                        </a>
+                      </StyledTableCell>
+                      <StyledTableCell align="center" sx={{ color: '#2F3A56' }}>
+                        {row.loan}
+                      </StyledTableCell>
+                      <StyledTableCell align="center" sx={{ color: '#2F3A56' }}>
+                        <Button
+                          variant="text"
+                          color="primary"
+                          sx={{
+                            backgroundColor: '#3A7E55',
+                            color: '#FFFFFF',
+                            margin: 1,
+                          }}
+                          onClick={handleClick}
+                        >
+                          Approve
+                        </Button>
+                        <Button
+                          variant="text"
+                          color="primary"
+                          sx={{
+                            backgroundColor: '#E0292A',
+                            color: '#FFFFFF',
+                            margin: 1,
+                          }}
+                        >
+                          Reject
+                        </Button>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </>
   )
 }
 
-export default DrawerAppBar
+export default Government
